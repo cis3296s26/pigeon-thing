3d4524207a88bdb9214b66b716fe1f86d071fafb
Luke-Lindsey <tur32628@temple.edu>
2026-03-24 11:56:50 -0400

    added shell of pigeon selector

--- lib/create_pigeon.dart
+++ lib/create_pigeon.dart
@@ -12,6 +12,32 @@ class _CreatePigeonState extends State<CreatePigeon> {
   final TextEditingController _messageController = TextEditingController();
   bool _isSending = false;
 
+  int _headIndex = 0;
+  int _torsoIndex = 0;
+  int _legsIndex = 0;
+
+  final List<String> heads = ['HEAD1', 'HEAD2', 'HEAD3'];
+  final List<String> torsos = ['TORSO1', 'TORSO2', 'TORSO3'];
+  final List<String> legs = ['LEGS1', 'LEGS2', 'LEGS3'];
+
+  void _createPigeon(){
+    final text = _messageController.text.trim();
+
+    setState(() {
+      _isSending = true;
+    });
+
+    _messageController.clear();
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text('Pigeon Created')),
+    );
+    
+    setState(() {
+      _isSending = false;
+    });
+  }
+
   Future<void> _sendMessage() async {
     final text = _messageController.text.trim();
 
@@ -57,6 +83,40 @@ class _CreatePigeonState extends State<CreatePigeon> {
   }
 }
 
+Widget piecesSelector({
+  required String label,
+  required List<String> items,
+  required int currentIndex,
+  required Function(int) onChanged,
+}) {
+  return Column(children: [
+    Text(label, style: const TextStyle(fontSize: 18)),
+    Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        IconButton(icon: const Icon(Icons.arrow_left), onPressed: () {
+          int newIndex = 
+          (currentIndex - 1 + items.length) % items.length;
+          onChanged(newIndex);
+          },
+        ),
+        Image.asset(items[currentIndex],
+        width: 100,
+        height: 100,
+        ),
+        IconButton(
+          icon: const Icon(Icons.arrow_right),
+          onPressed:(){
+            int newIndex = (currentIndex + 1) % items.length;
+            onChanged(newIndex);
+          },
+        ),
+      ],
+    )
+  ],
+  );
+}
+
   @override
   void dispose() {
     _messageController.dispose();
@@ -69,32 +129,74 @@ class _CreatePigeonState extends State<CreatePigeon> {
       appBar: AppBar(
         title: const Text('Create and Customize Your Pigeon Here!'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _messageController,
-              maxLines: 4,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Enter your pigeon message',
-                hintText: 'Type a message to upload to Firebase',
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              const Text('Preview', style: TextStyle(fontSize:20)),
+              const SizedBox(height: 10),
+              
+              SizedBox(
+                height: 220,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Image.asset(legs[_legsIndex], height: 200),
+                    Image.asset(torsos[_torsoIndex], height: 150),
+                    Image.asset(legs[_legsIndex], height: 100),
+                  ],
+                ),
               ),
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: _isSending ? null : _sendMessage,
-              child: Text(_isSending ? 'Sending...' : 'Send'),
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text('Go Back'),
-            ),
-          ],
+
+              const SizedBox(height: 20),
+
+              piecesSelector(
+                label: 'Head',
+                items: heads,
+                currentIndex: _headIndex,
+                onChanged: (i) => setState(() => _headIndex = i),
+              ),
+
+              piecesSelector(
+                label: 'Torso',
+                items: torsos,
+                currentIndex: _torsoIndex,
+                onChanged: (i) => setState(() => _torsoIndex = i),
+              ),
+
+              piecesSelector(
+                label: 'Legs',
+                items: legs,
+                currentIndex: _legsIndex,
+                onChanged: (i) => setState(() => _legsIndex = i),
+              ),
+
+              const SizedBox(height: 20),
+
+              TextField(
+                controller: _messageController,
+                maxLines: 4,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Enter your pigeon message',
+                  hintText: 'Type a message to upload to Firebase',
+                ),
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: _isSending ? null : _sendMessage,
+                child: Text(_isSending ? 'Sending...' : 'Send'),
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Go Back'),
+              ),
+            ],
+          ),
         ),
       ),
     );
